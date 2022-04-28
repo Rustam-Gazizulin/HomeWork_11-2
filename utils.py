bfdbb9676a2a59f0a11bc61a0d4e41fc03fef37d
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-04-28 13:40:32 +0500

    Добавил глобальную переменную __data чтобы сократить количество запросов к json файлу.

--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 import json
 
+__data = []
+
 
 def load_candidates_from_json(path):
     """возвращает список всех кандидатов"""
 
+    global __data
     with open(path, "r", encoding="utf-8") as file:
-        return json.load(file)
+        __data = json.load(file)
+        return __data
 
 def get_candidate(candidate_id):
     """возвращает одного кандидата по его id"""
 
-    candidates = load_candidates_from_json("candidates.json")
-    for candidate in candidates:
+    for candidate in __data:
         if candidate["id"] == candidate_id:
             return candidate
     return {'name': 'Ушел на обед и не вернулся:))'}  # уведомление на случай неверного id
@@ -19,17 +22,17 @@ def get_candidate(candidate_id):
 
 def get_candidates_by_name(candidate_name):
     """возвращает кандидатов по имени"""
-    candidates = load_candidates_from_json("candidates.json")
-    return [candidate for candidate in candidates if candidate_name.lower() in candidate["name"].lower()]
+
+    return [candidate for candidate in __data if candidate_name.lower() in candidate["name"].lower()]
 
 
 def get_candidates_by_skill(skill_name):
     """возвращает кандидатов по навыку"""
 
-    candidates = load_candidates_from_json("candidates.json")
+
     list_candidate_by_skill = []
 
-    for candidate in candidates:
+    for candidate in __data:
         if skill_name.lower() in candidate["skills"].lower().split(", "):
             list_candidate_by_skill.append(candidate)
 
